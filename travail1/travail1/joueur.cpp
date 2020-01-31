91c4ea8d1106247d1c97929e9aa33fc66a76c87e
Etienne Cloutier <etiennecloutier00@gmail.com>
2020-01-31 14:29:04 -0500

    ajout de class joueur

--- travail1/travail1/joueur.cpp
+++ travail1/travail1/joueur.cpp
@@ -1,2 +1,61 @@
 #include "joueur.h"
+Joueur::Joueur()
+{
+	nom = "";
+	nombrevictoires = 0;
+	nombreDefaites = 0;
+	for (int i = 0; i < maxCartesAJouer;i++)
+	{
+		mainDuJoueur[i] = NULL;
+	}
+}
 
+Joueur::Joueur(string inNom, int inNombreVictoires = 0, int inNombreDefaites = 0)
+{
+	nom = inNom;
+	nombrevictoires= inNombreVictoires;
+	nombreDefaites = inNombreDefaites;
+	for (int i = 0; i < maxCartesAJouer; i++)
+	{
+		mainDuJoueur[i] = NULL;
+	}
+}
+
+
+#pragma region Get
+
+string Joueur::getNom() const
+{
+	return nom;
+}
+int Joueur::getVictoires() const
+{
+	return nombrevictoires;
+}
+int Joueur::getDefaites() const
+{
+	return nombreDefaites;
+}
+
+#pragma endregion
+
+#pragma region fonctions
+
+void Joueur::setNom(string inNom)
+{
+	nom = inNom;
+}
+
+void Joueur::Pointage(bool inVictoire)
+{
+	if (inVictoire)
+	{
+		nombrevictoires++;
+	}
+	else 
+	{
+		nombreDefaites++;
+	}
+}
+
+#pragma endregion
--- travail1/travail1/joueur.h
+++ travail1/travail1/joueur.h
@@ -9,5 +9,31 @@ const int maxCartesAJouer = 26;
 class Joueur
 {
 
+
+private:
+	string nom;
+	int nombrevictoires;
+	int nombreDefaites;
+	Carte* mainDuJoueur[maxCartesAJouer];
+
+
+public:
+
+	Joueur();
+	Joueur(string nom, int nombreVictoires = 0, int nombreDefaites= 0);
+
+
+	string getNom() const;
+	int getVictoires() const;
+	int getDefaites() const;
+
+	void setNom(string Nom);
+
+	void Pointage(bool Victoire);
+
+
+	
+
+
 };
 #endif
